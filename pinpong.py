41141e1c32876b9d5f865ed92b11fa1741478186
Micha2009 <120248586+Micha2009@users.noreply.github.com>
2023-05-06 12:46:12 +0300

    Igra

---
+++ pinpong.py
@@ -0,0 +1,30 @@
+from curses import KEY_B2
+from random import randint
+from pygame import *
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < win_height -80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_Down] and self.rect.y < win_height -80:
+            self.rect.y += self.speed
